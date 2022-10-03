a8a66f3c6b0ce4440b0c6d2914b26b35c8aaf8fb
Elias de Souza <elias99-souza@hotmail.com>
2022-10-03 20:31:02 -0300

    ajustes nav

--- src/routs.js
+++ src/routs.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import NavBar from "./Components/NavBar"
 import Rodape from "./Components/Rodape"
 import Inicio from "./Pages/Inicio"
+import UncontrolledExample from "./Pages/Inicio/Carrosel"
 import Contato from "./Pages/SubPages/Contato"
 import Produtos from "./Pages/SubPages/Produtos"
 import Sobre from "./Pages/SubPages/Sobre"
@@ -9,6 +10,7 @@ import Sobre from "./Pages/SubPages/Sobre"
 export default function AppRoute () {
     return(
         <Router>
+            <UncontrolledExample/>
           <NavBar/>
             <Routes>
                 <Route path='/' element={<Inicio/>}  />
